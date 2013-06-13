5b0b473ed2d0f8ffa55565c5622a55b755e9f17c
Ronan Guilloux <ronan.guilloux@gmail.com>
2013-06-13 08:25:11 +0200

    test covering

--- tests/Rg/Tests/MarkdownServiceProviderTest.php
+++ tests/Rg/Tests/MarkdownServiceProviderTest.php
@@ -19,37 +19,52 @@ use Rg\Silex\Provider\Markdown\MarkdownServiceProvider;
 
 class MarkdownExtensionTest extends \PHPUnit_Framework_TestCase
 {
+
+    protected $app;
+    protected $mdPath;
+
     public function setUp()
     {
         if (!class_exists('Michelf\\Markdown')) {
             $this->markTestSkipped('Michelf Markdown Bundle was not installed.');
         }
+
+        $this->app = new Application();
+        $this->app['debug'] = true;
+        $this->mdPath = __DIR__ .'/../../resources/markdown';
+
     }
 
     public function testRegister()
     {
-        $app = new Application();
-        $app['debug'] = true;
-        $mdPath = __DIR__ .'/../../resources/markdown';
-        $app->register(new MarkdownServiceProvider(), array(
-            'md.path' => $mdPath)
+        $this->app->register(new MarkdownServiceProvider(), array(
+            'md.path' => $this->mdPath)
         );
 
-        $app->get('/', function() use($app) {
-            $markdown = $app['md.finder']->getContent(1);
+        $app = $this->app;
+        $this->app->get('/', function() use($app) {
+            $markdown = $this->app['md.finder']->getContent(1);
             if($markdown){
-                $html = $app['md.parser']->transform($markdown);
+                $html = $this->app['md.parser']->transform($markdown);
                 return $html;
             }
         });
         $request = Request::create('/');
-        $response = $app->handle($request);
-        $app->terminate($request, $response);
+        $response = $this->app->handle($request);
+        $this->app->terminate($request, $response);
 
-        $this->assertInstanceOf('\Michelf\MarkdownExtra', $app['md.parser']);
-        $expected = $app['md.parser']->transform($app['md.finder']->getContent(1));
+        $this->assertInstanceOf('\Michelf\MarkdownExtra', $this->app['md.parser']);
+        $expected = $this->app['md.parser']->transform($this->app['md.finder']->getContent(1));
         $this->assertContains($expected, $response->getContent());
+
+        $html = "<xml>" . $response->getContent() . "</xml>";
+        $dom = new \DOMDocument;
+        $dom->loadXml($html);
+        $title = $dom->getElementsByTagName('h1')->item(0)->nodeValue;
+        $this->assertEquals("Well title", $title);
+
     }
 }
+
 ?>
 
